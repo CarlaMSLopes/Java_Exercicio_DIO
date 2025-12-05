4112dbf0de24d0a15482ab7c1b6e5a40b8933d85
CarlaMSLopes <carlalopes613@outlook.com>
2025-12-05 14:44:58 -0300

    feat: tabuada

--- java-1/hello/src/App.java
+++ java-1/hello/src/App.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int numberUser = scanner.nextInt();
+        for(int i = 1; i <= 10; i++){
+            System.out.println(numberUser + " x " + i + " = " + numberUser * i);
+        }
+        
+        
+
     }
 }
