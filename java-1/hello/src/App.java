1f45becf97c5bddcd25e2f97c6bcd34b537835a3
CarlaMSLopes <carlalopes613@outlook.com>
2025-12-05 14:58:59 -0300

    feat: calculadora IMC

--- java-1/hello/src/App.java
+++ java-1/hello/src/App.java
@@ -10,7 +10,32 @@ public class App {
             System.out.println(numberUser + " x " + i + " = " + numberUser * i);
         }
         
+        System.out.println("Digite sua altura: ");
+        float altura = scanner.nextFloat();
+        System.out.println("Digite seu peso: ");
+        float peso = scanner.nextFloat();
+        float imc = peso / (altura * altura);
+
+        if(imc <= 18.5){
+            System.out.println("Abaixo do Peso");
+        }
+        else if(imc >= 18.6 && imc <= 24.9){
+            System.out.println("Peso Ideal");
+        }
+        else if(imc >= 25 && imc <= 29.9){
+            System.out.println("Levemente acima do peso");
+        }
+         else if(imc >= 30 && imc <= 34.9){
+            System.out.println("Obesidade Grau I");
+        }
+         else if(imc >= 35 && imc <= 39.9){
+            System.out.println("Obesidade Grau II (Severa)");
+        } else {
+            System.out.println("Obesidade III (Mórbida)");
+        }
+
         
+
 
     }
 }
